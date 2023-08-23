730030b9f02cd655b22b2e772563021b03dba631
FernandoPereiraSalvador <124562226+FernandoPereiraSalvador@users.noreply.github.com>
2023-08-23 13:04:26 +0200

    Views
    Se han creado las primeras vistas

--- ProyectoTienda/urls.py
+++ ProyectoTienda/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from ProyectoWebApp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home,name="Home"),
+    path('servicios', views.servicios,name="Servicios"),
+    path('tienda', views.tienda,name="Tienda"),
+    path('blog', views.blog,name="Blog"),
+    path('contacto', views.contacto,name="Contacto"),
 ]
--- ProyectoWebApp/views.py
+++ ProyectoWebApp/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+
 
 # Create your views here.
+
+def home(rquest):
+    return HttpResponse("Inicio")
+
+
+def servicios(rquest):
+    return HttpResponse("Servicios")
+
+
+def tienda(rquest):
+    return HttpResponse("Tienda")
+
+
+def blog(rquest):
+    return HttpResponse("Blog")
+
+
+def contacto(rquest):
+    return HttpResponse("Contacto")
