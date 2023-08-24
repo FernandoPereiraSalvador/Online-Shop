a9b01e0ed3c0596f9cc39358ebf4b576341d1109
FernandoPereiraSalvador <124562226+FernandoPereiraSalvador@users.noreply.github.com>
2023-08-24 21:20:17 +0200

    Corrección
    Ahora se puede visualizar las imagenes de servicios

--- ProyectoTienda/settings.py
+++ ProyectoTienda/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -105,7 +105,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-EU'
 
 TIME_ZONE = 'UTC'
 
@@ -118,6 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_URL='/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- ProyectoWebApp/urls.py
+++ ProyectoWebApp/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from ProyectoWebApp import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home,name="Home"),
@@ -8,3 +10,5 @@ urlpatterns = [
     path('blog', views.blog,name="Blog"),
     path('contacto', views.contacto,name="Contacto"),
 ]
+
+urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
