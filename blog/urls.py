851edfd0fad27670a1bad77c503970191ac5b6ee
FernandoPereiraSalvador <124562226+FernandoPereiraSalvador@users.noreply.github.com>
2023-08-25 17:17:38 +0200

    Filtrado de blog
    Se ha añadido una opción para filtrar el blog.

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.blog,name="Blog"),
+    path('categoria/<int:categoria_id>/',views.categoria,name="categoria")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-from blog.models import Post
+from blog.models import Post, Categoria
 # Create your views here.
 
 def blog(request):
-    post= Post.objects.all()
+    post = Post.objects.all()
     return render(request,"blog/blog.html",{"posts":post})
+
+def categoria(request,categoria_id):
+    categoria = Categoria.objects.get(id=categoria_id)
+    posts=Post.objects.filter(categorias=categoria)
+    return render(request,"blog/categoria.html",{'categoria':categoria, "posts":posts })
