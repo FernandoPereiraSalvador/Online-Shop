bd554d75029be1e2cdb45a02e0685d57ccec37ee
FernandoPereiraSalvador <124562226+FernandoPereiraSalvador@users.noreply.github.com>
2023-08-27 15:59:20 +0200

    Formato formulario
    Se ha mejorado el formato del formulario

--- contacto/forms.py
+++ contacto/forms.py
@@ -6,4 +6,5 @@ class FormularioContacto(forms.Form):
 
     email = forms.CharField(label="Email", required=True)
 
-    contenido = forms.CharField(label="Contenido")
+    contenido = forms.CharField(label="Contenido",widget=forms.Textarea)
+
