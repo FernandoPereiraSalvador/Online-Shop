edf2b4755140159742f25b4e4642328630cc691d
FernandoPereiraSalvador <124562226+FernandoPereiraSalvador@users.noreply.github.com>
2023-08-27 20:29:47 +0200

    Contacto
    Se ha hecho que la información del formulario de contacto se envíe al correo electrónico.
    
    Se ha creado la aplicación tienda.

--- ProyectoTienda/settings.py
+++ ProyectoTienda/settings.py
@@ -129,3 +129,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Configuración email
+
+EMAIL_HOST = 'sandbox.smtp.mailtrap.io'
+EMAIL_HOST_USER = '9326bfef12e2b3'
+EMAIL_HOST_PASSWORD = '9594b6e6b5376a'
+EMAIL_PORT = '2525'
+
