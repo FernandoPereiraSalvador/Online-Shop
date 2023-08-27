21bfcddd6fe975551b401cd7a5e8a64bc57d2804
FernandoPereiraSalvador <124562226+FernandoPereiraSalvador@users.noreply.github.com>
2023-08-27 22:10:00 +0200

    BD Tienda
    Se ha creado la base de datos de la tienda y se han creado unos datos de prueba

--- tienda/views.py
+++ tienda/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from .models import Producto
 
 
 # Create your views here.
 def tienda(request):
-    return render(request, "tienda/tienda.html")
+    productos = Producto.objects.all()
+
+    return render(request, "tienda/tienda.html",{"productos":productos})
