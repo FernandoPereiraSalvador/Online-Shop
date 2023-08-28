f187db400c784cc7cded2c44f7d076d7914a1a6c
FernandoPereiraSalvador <124562226+FernandoPereiraSalvador@users.noreply.github.com>
2023-08-28 15:59:34 +0200

    Vistas y botones
    Se han creado las vistas del carro de compra, los botones para agregar los productos y un contenedor que tendrá el carro de compra.

--- carro/views.py
+++ carro/views.py
@@ -1,3 +1,44 @@
 from django.shortcuts import render
+from .carro import Carro
+
+from tienda.models import Producto
+
+from django.shortcuts import redirect
 
 # Create your views here.
+def agregar_producto(request, producto_id):
+    carro = Carro(request)
+
+    producto = Producto.objects.get(id=producto_id)
+
+    carro.agregar(producto=producto)
+
+    return redirect("tienda")
+
+
+def eliminar_producto(request, producto_id):
+    carro = Carro(request)
+
+    producto = Producto.objects.get(id=producto_id)
+
+    carro.eliminar(producto=producto)
+
+    return redirect("tienda")
+
+def restar_producto(request, producto_id):
+    carro = Carro(request)
+
+    producto = Producto.objects.get(id=producto_id)
+
+    carro.restar_producto(producto=producto)
+
+    return redirect("tienda")
+
+def limpiar_producto(request, producto_id):
+    carro = Carro(request)
+
+    producto = Producto.objects.get(id=producto_id)
+
+    carro.limpiar_carro()
+
+    return redirect("tienda")
