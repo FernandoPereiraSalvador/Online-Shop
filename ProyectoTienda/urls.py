6e9ca5c2277b74360e192957f28795157912d2e1
FernandoPereiraSalvador <124562226+FernandoPereiraSalvador@users.noreply.github.com>
2023-08-29 09:27:30 +0200

    App autenticacion
    Se ha creado la app autenticacion

--- ProyectoTienda/urls.py
+++ ProyectoTienda/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('servicios/',include('servicios.urls')),
     path('blog/',include('blog.urls')),
     path('contacto/',include('contacto.urls')),
+    path('autenticacion/',include('autenticacion.urls')),
     path('tienda/',include('tienda.urls')),
     path('carro/',include('carro.urls')),
     path('', include('ProyectoWebApp.urls')),
