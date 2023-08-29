cfd39d246206434e4860ddc2ec480767be318da5
FernandoPereiraSalvador <124562226+FernandoPereiraSalvador@users.noreply.github.com>
2023-08-29 21:32:59 +0200

    Documentación
    Se ha documentado el archivo configuracion.py y se a ajustado la documentación del archivo main

--- autenticacion/views.py
+++ autenticacion/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.contrib import messages
 # Create your views here.
 class VRegistro(View):
 
@@ -9,4 +11,17 @@ class VRegistro(View):
         return render(request,"registro/registro.html",{"form":form})
 
     def post(self,request):
-        pass
+        form=UserCreationForm(request.POST)
+
+        if form.is_valid():
+
+            usuario = form.save()
+
+            login(request,usuario)
+
+            return redirect('Home')
+
+        else:
+            for msg in form.error_messages:
+                messages.error(request,form.error_messages[msg])
+            return render(request, "registro/registro.html", {"form": form})
