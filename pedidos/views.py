9a43b54b76ce835449c944fd65ed5a1db3fd8917
FernandoPereiraSalvador <124562226+FernandoPereiraSalvador@users.noreply.github.com>
2023-09-04 16:20:33 +0200

    Pedidos vista
    Se han creado las vistas de la aplicación pedidos.

--- pedidos/views.py
+++ pedidos/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.shortcuts import render, redirect
 
 # Create your views here.
+from carro.carro import Carro
+from pedidos.models import Pedido, LineaPedido
+
+
+@login_required(login_url="")
+def procesar_pedido(request):
+    pedido = Pedido.objects.create(user=request.user)
+    carro = Carro(request)
+    lineas_pedido=list()
+    for key, value in carro.carro.items():
+        lineas_pedido.append(LineaPedido(
+            producto_id=key,
+            cantidad=value["cantidad"],
+            user = request.user,
+            pedido = pedido
+        ))
+
+    LineaPedido.objects.bulk_create(lineas_pedido)
+
+    enviar_mail(
+        pedido = pedido,
+        lineas_pedido = lineas_pedido,
+        nombreusuario = request.username,
+        emailusuario=request.usermail,
+    )
+
+    messages.success(request,"El pedido se ha creado correctamente")
+
+    return redirect("../tienda")
