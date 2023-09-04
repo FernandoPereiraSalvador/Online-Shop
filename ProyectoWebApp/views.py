e9989fe91a8c6282bd98ba808dfafb82760293a9
FernandoPereiraSalvador <124562226+FernandoPereiraSalvador@users.noreply.github.com>
2023-09-04 13:36:18 +0200

    Correccion de errores y app pedidoss
    Se han corregido varios errores de la aplicación del carro.
    
    También se ha creado la app pedidos

--- ProyectoWebApp/views.py
+++ ProyectoWebApp/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+from carro.carro import Carro
+
 
 def home(request):
+
+    carro = Carro(request)
+
     return render(request,"home.html")
 
--- carro/context_processor.py
+++ carro/context_processor.py
@@ -1,8 +1,10 @@
 def importe_total_carro(request):
     total = 0
-
-    if "carro" in request.session:
+    if request.user.is_authenticated:
         for key, value in request.session["carro"].items():
             total += float(value["precio"])
+
+    else:
+        total = "Necesitas iniciar sesión"
 
     return {"importe_total_carro": total}
