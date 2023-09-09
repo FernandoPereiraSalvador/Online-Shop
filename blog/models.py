52123260e8986840331f6961b82a9218c881f176
FernandoPereiraSalvador <124562226+FernandoPereiraSalvador@users.noreply.github.com>
2023-09-09 12:59:49 +0200

    Comentarios
    Se ha creado un sistema en el que los usuarios pueden agregar comentarios a los post

--- blog/models.py
+++ blog/models.py
@@ -42,3 +42,17 @@ class Imagen(models.Model):
     imagen = models.ImageField(upload_to='blog/imagenes/')
     titulo = models.CharField(max_length=100)
     descripcion = models.TextField(blank=True)
+
+
+class Comentario(models.Model):
+    autor = models.ForeignKey(User, on_delete=models.CASCADE)
+    texto = models.TextField()
+    fecha = models.DateTimeField(auto_now_add=True)
+    publicacion = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'comentario'
+        verbose_name_plural = 'comentarios'
+
+    def __str__(self):
+        return f"Comentario de {self.autor.username} en {self.publicacion.titulo}"
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render, get_object_or_404
-from blog.models import Post, Categoria
+from django.core.checks import messages
+from django.shortcuts import render, get_object_or_404, redirect
+
+from blog.forms import ComentarioForm
+from blog.models import Post, Categoria, Comentario
 # Create your views here.
 
 def blog(request):
@@ -11,6 +14,30 @@ def categoria(request,categoria_id):
     posts=Post.objects.filter(categorias=categoria)
     return render(request,"blog/categoria.html",{'categoria':categoria, "posts":posts })
 
+
+from django.shortcuts import render, get_object_or_404, redirect
+from blog.models import Post
+from .models import Comentario
+from .forms import ComentarioForm
+
+
 def detalle_articulo(request, post_id):
     post = get_object_or_404(Post, id=post_id)
-    return render(request, 'blog/detalle_articulo.html', {'post': post})
+    comentarios = Comentario.objects.filter(publicacion=post)
+
+    if request.method == 'POST':
+        if request.user.is_authenticated:
+            formulario = ComentarioForm(request.POST)
+            if formulario.is_valid():
+                nuevo_comentario = formulario.save(commit=False)
+                nuevo_comentario.autor = request.user
+                nuevo_comentario.publicacion = post
+                nuevo_comentario.save()
+                return redirect('detalle_articulo', post_id=post_id)
+        else:
+            messages.error(request, 'Necesitas iniciar sesión para dejar un comentario.')
+    else:
+        formulario = ComentarioForm()
+
+    return render(request, 'blog/detalle_articulo.html',
+                  {'post': post, 'comentarios': comentarios, 'formulario': formulario})
