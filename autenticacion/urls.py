5a276b7063515cf40b70e27fc83f37f2b1e0a177
FernandoPereiraSalvador <124562226+FernandoPereiraSalvador@users.noreply.github.com>
2023-09-14 17:25:10 +0200

    Panel de usuario
    Se ha creado el panel de usuario para administrar la cuenta. Además se ha agregado  una forma de ver y eliminar los comentarios del usuario desde su panel.

--- autenticacion/urls.py
+++ autenticacion/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from ProyectoWebApp import views
 from . import views
-from .views import VRegistro, cerrar_sesion, logear
+from .views import VRegistro, cerrar_sesion, logear,panel_usuario
 
 urlpatterns = [
     path('', VRegistro.as_view(), name="Autenticacion"),
-    path('cerrar_sesion', cerrar_sesion, name="cerrar_sesion"),
+    path('cerrar sesion', cerrar_sesion, name="cerrar_sesion"),
     path('logear', logear, name="logear"),
+    path('panel usuario',panel_usuario, name="panel_usuario"),
+    path('eliminar_comentario/<int:comentario_id>/', views.eliminar_comentario, name='eliminar_comentario'),
 ]
--- autenticacion/views.py
+++ autenticacion/views.py
@@ -1,51 +1,77 @@
-from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import View
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 # Create your views here.
+from blog.models import Comentario
+
+
 class VRegistro(View):
 
-    def get(self,request):
-        form=UserCreationForm()
-        return render(request,"registro/registro.html",{"form":form})
+    def get(self, request):
+        form = UserCreationForm()
+        return render(request, "registro/registro.html", {"form": form})
 
-    def post(self,request):
-        form=UserCreationForm(request.POST)
+    def post(self, request):
+        form = UserCreationForm(request.POST)
 
         if form.is_valid():
 
             usuario = form.save()
 
-            login(request,usuario)
+            login(request, usuario)
 
             return redirect('Home')
 
         else:
             for msg in form.error_messages:
-                messages.error(request,form.error_messages[msg])
+                messages.error(request, form.error_messages[msg])
             return render(request, "registro/registro.html", {"form": form})
+
 
 def cerrar_sesion(request):
     logout(request)
     return redirect('Home')
 
-def logear(request):
 
-    if request.method=="POST":
-        form=AuthenticationForm(request,data=request.POST)
+def logear(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
             nombre_usuario = form.cleaned_data.get("username")
-            contraseña=form.cleaned_data.get("password")
+            contraseña = form.cleaned_data.get("password")
 
-            usuario = authenticate(username=nombre_usuario,password=contraseña)
+            usuario = authenticate(username=nombre_usuario, password=contraseña)
             if usuario is not None:
-                login(request,usuario)
+                login(request, usuario)
                 return redirect('Home')
             else:
-                messages.error(request,"Usuario no válido")
+                messages.error(request, "Usuario no válido")
         else:
-            messages.error(request,"Información incorrecta")
+            messages.error(request, "Información incorrecta")
 
     form = AuthenticationForm()
-    return render(request,"login/login.html",{"form":form})
+    return render(request, "login/login.html", {"form": form})
+
+
+@login_required
+def panel_usuario(request):
+    # Obtener información del usuario autenticado
+    usuario = request.user
+
+    # Consultar los comentarios del usuario
+    comentarios = Comentario.objects.filter(autor=usuario)
+    return render(request, 'panel usuario/panel_usuario.html', {'usuario': usuario, 'comentarios': comentarios})
+
+
+def eliminar_comentario(request, comentario_id):
+    usuario = request.user
+    comentarios = Comentario.objects.filter(autor=usuario)
+
+    comentario = get_object_or_404(Comentario, id=comentario_id)
+    if request.method == 'POST':
+        comentario.delete()
+    return render(request,'panel usuario/panel_usuario.html', {'usuario': usuario, 'comentarios': comentarios})
+
